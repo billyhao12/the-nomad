48b1f9bb5cd267a6b53d3675d32bc7fb03f941d1
Christopher Marti <chrisjm093@gmail.com>
2020-08-25 21:17:29 -0700

    CRUD Article controller built

---
+++ controllers/articleController.js
@@ -0,0 +1,29 @@
+const { decodeBase64 } = require("bcryptjs")
+
+module.exports = {
+  findAll: function(req, res) {
+    db.Article
+      .find(req.query)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
+  },
+  create: function(req, res) {
+    db.Article
+    .create(req.body)
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err))
+  },
+  update: function(req, res) {
+    db.Article
+    .findOneAndUpdate({ _id: req.params.id }, req.body)
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err))
+  },
+  remove: function(req, res) {
+    db.Article
+    .findById({ _id: req.params.id })
+    .then(dbModel => dbModel.remove())
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err))
+  }
+}
