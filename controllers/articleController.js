3c17a4451586bc2fa038a3f0020c97509a38021f
Christopher Marti <chrisjm093@gmail.com>
2020-08-25 21:45:14 -0700

    CRUD Controller for user profile

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -4,6 +4,7 @@ module.exports = {
   findAll: function(req, res) {
     db.Article
       .find(req.query)
+      .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err))
   },
