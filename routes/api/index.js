c985d35093254c15010891bccc1a07e488e5a6d8
Christopher Marti <chrisjm093@gmail.com>
2020-08-27 14:29:23 -0700

    Configured api Index routes to handle article, comment, and profile API calls

---
+++ routes/api/index.js
@@ -0,0 +1,15 @@
+const router = require ('express').Router();
+const articleRoutes = require('./articles');
+const commentRoutes = require('./comments');
+const profileRoutes = require('./profiles');
+
+// Article Routes
+router.use('/articles', articleRoutes);
+
+// Comment Routes
+router.use('/comments', commentRoutes);
+
+// Profile Routes
+router.use('/profiles', profileRoutes);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const path = require('path');
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+// API routes
 router.use('/api', apiRoutes);
 
+//If no API routes are hit, send the React app
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, '../client/build/index.html'))
 });
