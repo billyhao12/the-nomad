116132d17095d02f142df94184edcc1ca2a62e69
Christopher Marti <chrisjm093@gmail.com>
2020-08-28 16:21:43 -0700

    uncommented commented out code

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,15 +1,15 @@
 const router = require ('express').Router();
 const articleRoutes = require('./articles');
-// const commentRoutes = require('./comments');
-// const profileRoutes = require('./profiles');
+const commentRoutes = require('./comments');
+const profileRoutes = require('./profiles');
 
 // Article Routes
 router.use('/articles', articleRoutes);
 
-// // Comment Routes
-// router.use('/comments', commentRoutes);
+// Comment Routes
+router.use('/comments', commentRoutes);
 
-// // Profile Routes
-// router.use('/profiles', profileRoutes);
+// Profile Routes
+router.use('/profiles', profileRoutes);
 
 module.exports = router;
