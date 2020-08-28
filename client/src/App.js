ec8e7df4b505fac257f99bc9d66cd122fd3f3175
Christopher Marti <chrisjm093@gmail.com>
2020-08-27 20:25:47 -0700

    commit to pull

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-
 import { useAuthTokenStore } from './utils/auth';
-
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -15,9 +13,12 @@ function App() {
     <div className="App">
       <Router>
         <div>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          
+          </Switch>
         </div>
       </Router>
     </div>
