51d9aea2cf665a2e542e4dc254cb58d396029da3
Tcreamer <trentoncreamer@yahoo.com>
2020-08-29 13:46:27 -0700

    Fixed comment schema

--- models/Comment.js
+++ models/Comment.js
@@ -2,18 +2,14 @@ const mongoose = require ('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-  user: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-  article: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Article",
-    },
-  ],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  article: {
+    type: Schema.Types.ObjectId,
+    ref: "Article",
+  },
   author: {
     type: String,
     required: true,
