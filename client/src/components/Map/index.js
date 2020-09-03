5da3b46632524d0d59b50a45c79c839b842b5e1b
Christopher Marti <chrisjm093@gmail.com>
2020-09-02 19:44:53 -0700

    Mapbox article locations rendering to map with clusters and heat map.

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -8,19 +8,29 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY2hyaXNqbTA5MyIsImEiOiJja2VkZHFsMjIwMnRrMnBud
 
 function Map(props){
   
-
-  let articlesCoordinates = [props.articlesCoordinates]
-
-  console.log(articlesCoordinates);
   const [state, setState] = useState({lng: -122.51, lat: 47.62, zoom: 7.89});
  
   const mapContainerRef = useRef()
 
   useEffect(() =>{
-    if(!props.userLatitude){
+    if(!props.userLatitude || !props.articlesCoordinates){
       return
     }
 
+    const articlesGeoJSON = {
+      'type': 'FeatureCollection',
+      'crs': {
+        'type': 'name',
+        'properties': {
+          'name': 'TheNomadArticles'
+        }
+      },
+      'features':props.articlesCoordinates
+    }
+   
+
+    console.log(articlesGeoJSON)
+  
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: 'mapbox://styles/mapbox/streets-v11',
@@ -28,19 +38,85 @@ function Map(props){
       zoom: 8
     });
 
-  
+    map.addControl(new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true
+    }));
+
     map.on('load',  function() { 
+      //Source for geoJSON data is in articlesCoordinates and formatted with articlesGeoJSON object
+      //cluster option is set to true
+      map.addSource('articles', {
+        type: 'geojson',
+        data: articlesGeoJSON,
+        cluster: true,
+        clusterMaxZoom: 5, //Max zoom to cluster points on
+        clusterRadius: 50 //Radius of each cluster when clustering points (50 is the default)
+      });
+
+      map.addLayer({
+        id: 'clusters',
+        type: 'circle',
+        source: 'articles',
+        filter: ['has', 'point_count'],
+        paint: {
+          //steps indicate at which level of data points a specified circle will be drawn
+          //less than 3 data points = blue
+          //3-7 data points = yellow
+          //greater than 7 points = pink
+          'circle-color':[
+            'step',
+            ['get', 'point_count'],
+            '#51bbd6',
+            3,
+            '#f1f075',
+            7,
+            '#f28cb1'
+          ],
+          'circle-radius': [
+            'step',
+            ['get', 'point_count'],
+            20,
+            3,
+            30,
+            7,
+            40
+          ]
+        }
+      });
+
+      map.addLayer({
+        id: 'cluster-count',
+        type: 'symbol',
+        source: 'articles',
+        filter: ['has', 'point_count'],
+        layout: {
+          'text-field': '{point_count_abbreviated}',
+          'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+          'text-size': 12
+        }
+      });
+
+      map.addLayer({
+        id: 'unclustered-point',
+        type: 'circle',
+        source: 'articles',
+        filter: ['!', ['has', 'point_count']],
+        paint: {
+          'circle-color': '#11b4da',
+          'circle-radius': 4,
+          'circle-stroke-width': 1,
+          'circle-stroke-color': '#fff'
+        }
+      });
 
       new mapboxgl.Marker()
         .setLngLat({lon: props.userLongitude, lat: props.userLatitude})
         .addTo(map)
 
-      map.addSource('articles', {
-        'type': 'geojson',
-        'data': '../articles/'
-        //insert correct link to articles geojason data in quotes above
-      });
-
+     
       map.addLayer(
         {
           'id': 'articles-heat',
@@ -166,11 +242,13 @@ function Map(props){
 
 
     map.on('move', () => {
+      
       setState({
         lng: map.getCenter().lng.toFixed(4),
         lat: map.getCenter().lat.toFixed(4),
         zoom: map.getZoom().toFixed(2)
       });
+    
     });
 
   },[props])
