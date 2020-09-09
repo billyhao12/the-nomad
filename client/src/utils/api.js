ef010875f143dbd1315d608756320fa37f7c7c95
Tcreamer <trentoncreamer@yahoo.com>
2020-09-08 21:02:51 -0700

    commit to pull

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,43 +1,52 @@
 import axios from 'axios';
 
 export default {
+  getComments() {
+    return axios.get('/api/comments');
+  },
+  createComment(data) {
+    return axios.post('/api/comments', data);
+  },
+  getComment(id) {
+    return axios.get('/api/comments' + id);
+  },
+   updateComment(id) {
+    return axios.get('/api/comments' + id);
+  },
+   deleteComment(id) {
+    return axios.delete('/api/comments' + id);
+  },
+
 
   getArticles() {
     return axios.get('/api/articles');
   },
 
-  getArticle( id ) {
+  getArticle(id) {
     return axios.get('/api/articles/' + id);
   },
 
-  getArticleCat( cat ) {
+  getArticleCat(cat) {
     return axios.get('/api/articles/category/' + cat);
   },
 
-  createArticle( data ) {
+  createArticle(data) {
     return axios.post('/api/articles', data);
   },
 
-  getUser( id ) {
+  getUser(id) {
     return axios.get('/api/profiles/' + id);
   },
 
-  register( userData ) {
-
+  register(userData) {
     return axios.post('/api/register', userData);
-
   },
 
-  login( userData ) {
-
+  login(userData) {
     return axios.post('/api/login', userData);
-
   },
 
   authenticated() {
-
     return axios.post('/api/authenticated');
-
-  }
-
-}
+  },
+};
