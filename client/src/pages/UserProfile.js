f433367745de6045f5fba3467ae59a46bd1aac30
anthnsm <anthony.smith353@gmail.com>
2020-09-08 19:40:44 -0700

    can get user data into the user profile page. fixed profilesController

---
+++ client/src/pages/UserProfile.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {useStoreContext} from '../store';
+
+import api from '../utils/api';
+
+
+
+function UserProfile(props) {
+  const [user, setUser] = useState(null);
+  const [{userAuth}] = useStoreContext();
+
+  useEffect(() => {
+    loadUser(userAuth);
+  },[userAuth]);
+
+  
+  function loadUser(auth) {
+    if(auth) {
+      api.getUser(auth.id)
+        .then(res => {
+          setUser(res.data);
+        })
+        .catch(err => console.log(err));
+    }
+    else
+      console.log('undef');
+    
+  }
+
+  if(user) {
+    console.log(user);
+    return (
+      <div>
+        {user._id}
+      </div>
+    )
+  }
+  else {
+    return (
+      <p>User Profile Page</p>
+    );
+  }
+}
+
+export default UserProfile;
