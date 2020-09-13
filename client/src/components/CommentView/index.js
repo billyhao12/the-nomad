e49f9ce719cc56cb28fdc9115e604a51e24e052e
Christopher Marti <chrisjm093@gmail.com>
2020-09-13 12:56:56 -0700

    Commenter name now renders in comments

--- client/src/components/CommentView/index.js
+++ client/src/components/CommentView/index.js
@@ -5,20 +5,33 @@ import { Media, Content, Level, Button} from 'react-bulma-components';
 
 import api from '../../utils/api';
 
+
 function CommentView(props) {
 
   const [comment, setComment] = useState();
+  const [user, setUser] = useState();
+
 
   useEffect(() => {
     getComment(props.commentId);
   },[]);
 
+  function loadUser(comment) {
+    api.getUser(comment.user)
+      .then(res=> {
+        setUser(res.data)
+        setComment(comment)} )
+      .catch(err => console.log(err))
+  }
+
   function getComment(id) {
     api.getComment(id)
       .then(res => {
-        setComment(res.data);
+        loadUser(res.data);
       })
   }
+
+
 
   if(comment) {
     return (
@@ -26,7 +39,7 @@ function CommentView(props) {
         <Media.Item>
           <Content>
             <p>
-              <strong>commenter name (_id): {comment.user}</strong> <small>Commented: {comment.date}</small>
+              <strong>{user.name} says:</strong> <small>Commented: {comment.date}</small>
               <br />
               {comment.content}
             </p>
